34461d46ce03fda33ea5b37689020c24da22ff37
ShubhamD32 <40306335+ShubhamD32@users.noreply.github.com>
2021-02-01 06:38:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import ListItems from './Components/ListItems';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faTrash);
 
 export default class App extends Component {
 	constructor(props) {
 		super(props);
+
+		//To persist todo items in local storage between browser refreshes
+		let localStorageItems = JSON.parse(window.localStorage.getItem('items'));
+		localStorageItems = localStorageItems !== null ? localStorageItems : [];
+
+		//Setting initial state
 		this.state = {
-			items: [],
+			items: localStorageItems,
 			currentItem: {
 				text: '',
 				key: '',
@@ -18,10 +20,18 @@ export default class App extends Component {
 		};
 	}
 
+	// Persist state value to browser localStorage when component updates
+	componentDidUpdate() {
+		window.localStorage.setItem('items', JSON.stringify(this.state.items));
+	}
+
 	handleInput = (e) => {
 		this.setState({
 			currentItem: {
 				text: e.target.value,
+
+				// Selects and assigns the current date timestamp as the unique ID for the todo
+				// 'uuid' library can be used here to implement the same functionality more reliably
 				key: Date.now(),
 			},
 		});
@@ -29,7 +39,10 @@ export default class App extends Component {
 
 	addItem = (e) => {
 		e.preventDefault();
+		// Funcion to be called when the current Item is to be added to the items state array
 		const newItem = this.state.currentItem;
+
+		//Does nothing if currentItem.text is empty => Disallows blank todos from being added
 		if (newItem.text !== '') {
 			const newItems = [...this.state.items, newItem];
 			this.setState({
@@ -43,15 +56,22 @@ export default class App extends Component {
 	};
 
 	deleteHandler = (key) => {
+		// Filters all items where item->key is not equal to the value passed as a parameter to the function.
+
+		// Removes the element to be deleted from the state array of items
 		const filteredItems = this.state.items.filter((item) => item.key !== key);
+		// Update the items array in state with the updated array after the item has been deleted
 		this.setState({
 			items: filteredItems,
 		});
 	};
 
 	setUpdateHandler = (updatedText, key) => {
+		// Make a copy of the state items array
 		const items = [...this.state.items];
 		items.forEach((item) => {
+			/* While mapping over the items array, if the key of the item === the key passed to the function, the value of the text property of the item is set to the updatedText value passed to the function */
+
 			if (item.key === key) {
 				item.text = updatedText;
 			}
@@ -70,13 +90,17 @@ export default class App extends Component {
 							<input
 								type='text'
 								placeholder='Enter text'
+								/*Initially, the value is an empty string, it updates as characters are typed into the input field. For each keystroke, the state value of the currentItem object is updated.*/
 								value={this.state.currentItem.text}
+								// Updates state value per keystroke
 								onChange={this.handleInput}
 							/>
 							<button type='submit'>Add</button>
 						</form>
 					</header>
 					<ListItems
+						/* Passing down functions to the ListItems component to be used as event listeners in the individual todo items.*/
+
 						items={this.state.items}
 						deleteHandler={this.deleteHandler}
 						setUpdateHandler={this.setUpdateHandler}
--- src/Components/ListItems.js
+++ src/Components/ListItems.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import './ListItems.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+// To use trash icon while displaying todos
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// To use transitions while displaying/deleting todos
 import FlipMove from 'react-flip-move';
 
+// To use font-awesome trash icon
+library.add(faTrash);
+
+// Maps over all the items in the items array and displays each element in its own <div> tag
 function ListItems(props) {
 	const items = props.items;
+	// Maps over all the items in the items array and displays each element in its own <div> tag
 	const listItems = items.map((item) => {
 		return (
+			// Adding <div> tags and event listeners to all the items present in the items state array
+
 			<div className='list' key={item.key}>
 				<p>
 					<input
 						type='text'
 						id={item.key}
 						value={item.text}
+						// Functionality to change the text of a single Todo item
+						// Passes the text to be changed and the unique ID of the todo item
 						onChange={(e) => {
 							props.setUpdateHandler(e.target.value, item.key);
 						}}></input>
@@ -21,6 +36,7 @@ function ListItems(props) {
 							onClick={() => {
 								props.deleteHandler(item.key);
 							}}
+							// Trash icon imported from font-awesome
 							className='faIcons'
 							icon='trash'
 						/>
@@ -30,6 +46,7 @@ function ListItems(props) {
 		);
 	});
 	return (
+		// Flip move library is used for the transition effect when displaying and removing todos
 		<div>
 			<FlipMove duration={500} easing='ease-in-out'>
 				{listItems}
